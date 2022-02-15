cf2a35c099a8146f7b0f6794ad19b96613cbc77e
EROR <81974347+ElierRosales@users.noreply.github.com>
2022-02-15 16:03:51 -0600

    Test servo condicionantes

---
+++ servoTest1.py
@@ -0,0 +1,38 @@
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Autor: Juan Elier Rosales Rosas
+Fecha: 11/02/22
+Descripción: Programa para mover un servomotor simulando la apertura de una puerta
+	
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+###################################
+#Declaración de modulos
+###################################
+import RPi.GPIO as GPIO
+import time 
+
+###################################
+#Configuración de GPIO
+###################################
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT) #Se configura el pin 12 como salida
+servo = GPIO.PWM(11, 50) # 12 es el pin y 50 el pulso en Hz
+
+#Declaración de variables
+servo.start(0)
+abierto = int(input("Esperando: "))
+
+if (abierto == 1):
+
+    print("Puerta abierta")
+    servo.ChangeDutyCycle(7)
+    time.sleep(2)
+    
+elif(abierto == 0):
+    print("Puerta cerrada")
+    servo.ChangeDutyCycle(2)
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(0)
+
+servo.stop()
+GPIO.cleanup()
